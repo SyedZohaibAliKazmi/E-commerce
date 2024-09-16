6c2d909c680a03b1a0e89c801c45390bb9a36559
syedzohaibalikazmi <syedzohaibali9900@gmail.com>
2024-09-17 04:59:56 +0500

    E-Commerce

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,41 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faCartShopping, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
-      {/* 1 div  */}
       <div className="main-navbar">
-        {/* 2 div  */}
         <div className="logo">
           <h1>Logo</h1>
         </div>
-        {/* 3 div  */}
-        <div className="pages-link">
-            <ul>
-                <li><Link to="/Home">Home</Link></li>
-                <li><Link to="/Product">Products</Link></li>
-                <li><Link to="About">About</Link></li>
-                <li><Link to="/Contact">Contact</Link></li>
-            </ul>
+        <div className={`pages-link ${isOpen ? "open" : ""}`}>
+          <ul>
+            <li><Link to="/Home">Home</Link></li>
+            <li><Link to="/Product">Products</Link></li>
+            <li><Link to="/About">About</Link></li>
+            <li><Link to="/Contact">Contact</Link></li>
+          </ul>
         </div>
-
-        {/* 4 div  */}
-        {/* <div className="Navbar-Cart">
-        <button className="cart-button"><FontAwesomeIcon icon={faCartShopping}/></button>
-        </div> */}
-
-
-        {/* 5 div  */}
         <div className="Navbar-Account">
-        <button className="cart-button"><FontAwesomeIcon icon={faCartShopping}/></button>
-          <button  className="account-button"><FontAwesomeIcon icon={faUser}/> </button>
+          <button className="cart-button">
+            <FontAwesomeIcon icon={faCartShopping} />
+          </button>
+          <button className="account-button">
+            <FontAwesomeIcon icon={faUser} />
+          </button>
         </div>
+        <button className="hamburger" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
+        </button>
       </div>
     </div>
   );
