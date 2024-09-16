2f2d1f8e4b18c00097a137d41e5c64c35e909e3c
syedzohaibalikazmi <syedzohaibali9900@gmail.com>
2024-09-16 17:32:05 +0500

    E-Commerce

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,8 @@
+function Contact() {
+
+    return(
+        <h1>Welcome! Contact Page </h1>
+    )
+    
+}
+export default Contact
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import './styles/Navbar.css';
+
+function Navbar() {
+
+    return(
+        <div >
+            {/* 1 div  */}
+            <div className='main-navbar'> 
+                <div className='logo'><h1>Logo</h1></div> 
+
+            </div>
+            
+        </div>
+    )
+    
+}
+
+export default Navbar
